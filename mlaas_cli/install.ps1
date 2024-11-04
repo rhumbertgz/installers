9f6a221f5da301d6d1dd049fb4f1de2b071d32e3
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-11-04 13:35:05 +0100

    chore: remove code related to update the PATH env

--- mlaas_cli/install.ps1
+++ mlaas_cli/install.ps1
@@ -175,23 +175,6 @@ function Unblock-Cli {
             Log-Info "MLaaS CLI unblocked successfully."
         } 
 
-        # Get the current PATH environment variable
-        $CURRENT_PATH = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
-
-        # Check if the path is already in the PATH variable
-        if ($CURRENT_PATH -notlike "*$BINARY_PATH*") {
-            # Add the new path to the PATH variable
-            $NEW_PATH = "$CURRENT_PATH;$BINARY_PATH"
-            [System.Environment]::SetEnvironmentVariable("Path", $NEW_PATH, [System.EnvironmentVariableTarget]::Machine)
-            if ($?){
-                Log-Info "CLI path added to PATH environment variable."
-            } else {
-                Log-Info "CLI path could not be added to PATH environment variable."
-            }
-        } else {
-            Log-Info "CLI path is already in the PATH environment variable."
-        }
-
         Log-Info "The CLI is installed at: $MLAAS_PATH"    
     }   
 }
