a93c4d106eaeaf8f4285449c851f4cb9323375ca
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-11-06 17:44:40 +0100

    fix: remove extra espace

--- mlaas_cli/install.ps1
+++ mlaas_cli/install.ps1
@@ -82,7 +82,7 @@ function Install-Python {
         $OPTIONS = &("3.11.0". "3.12.0". "3.13.0", "latest")
 
         for ($i = 0; $i -lt $OPTIONS.Count; $i++){
-            Write-Output "$($i + 1). $($OPTIONS [$i])"
+            Write-Output "$($i + 1). $($OPTIONS[$i])"
         }
 
         $SELECTION = Read-Host "Please select an option (1-$($OPTIONS.Count))"
