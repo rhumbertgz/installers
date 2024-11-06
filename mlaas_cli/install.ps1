9660c5d7018b29b8a72c4ff3fc65ffb33404906b
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-11-06 17:51:10 +0100

    fix: options list

--- mlaas_cli/install.ps1
+++ mlaas_cli/install.ps1
@@ -79,7 +79,7 @@ function Verify-PipxInstallation{
 
 function Install-Python {
     try {
-        $OPTIONS = &("3.11.0". "3.12.0". "3.13.0", "latest")
+        $OPTIONS = &("3.11.0", "3.12.0", "3.13.0", "latest")
 
         for ($i = 0; $i -lt $OPTIONS.Count; $i++){
             Write-Output "$($i + 1). $($OPTIONS[$i])"
