ea116fef654fa1a78e173c343d6f2c0512b6d81e
Rodriguez Avila Humberto <humberto.rodriguez_avila@nokia-bell-labs.com>
2024-11-06 17:32:14 +0100

    fix: remove invalid UTF8 char

--- mlaas_cli/install.ps1
+++ mlaas_cli/install.ps1
@@ -81,7 +81,7 @@ function Install-Python {
     try {
         $OPTIONS = &("3.11.0". "3.12.0". "3.13.0", "latest")
 
-        for （$i = 0; $i -lt $OPTIONS.Count; $i++）{
+        for ($i = 0; $i -lt $OPTIONS.Count; $i++){
             Write-Output "$($i + 1). $($OPTIONS [$i])"
         }
 
